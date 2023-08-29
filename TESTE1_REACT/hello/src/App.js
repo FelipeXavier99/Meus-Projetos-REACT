23b845f3f691d373dae913c051d185da382e9616
Felipe Xavier <felipe.cvxavier@gmail.com>
2023-08-29 14:59:52 -0300

    up

--- TESTE1_REACT/hello/src/App.js
+++ TESTE1_REACT/hello/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>oiii</h1>
-      <h2>meee</h2>
-    </div>
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
   );
 }
-
-export default App;
